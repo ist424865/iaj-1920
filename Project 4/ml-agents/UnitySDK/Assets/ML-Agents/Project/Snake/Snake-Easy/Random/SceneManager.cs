d8668296f9f729e8a85149ebddb5c46d3764012f
Rui Ribeiro <ruinunca@icloud.com>
2020-01-14 19:32:26 +0000

    Add local ml-agents

---
+++ Project 4/ml-agents/UnitySDK/Assets/ML-Agents/Project/Snake/Snake-Easy/Random/SceneManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using MLAgents;
+
+public class SceneManager : MonoBehaviour {
+
+    public GameObject apple;
+    public const int APPLE_NUMBER = 5;
+
+    // Use this for initialization
+    void Start () {
+        for (int i = 0; i < APPLE_NUMBER; i++)
+        {
+            var clone = GameObject.Instantiate(apple);
+            clone.transform.position = new Vector3(Random.Range(-7.0f, 7.0f), 0, Random.Range(-7.0f, 7.0f));
+        }
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
