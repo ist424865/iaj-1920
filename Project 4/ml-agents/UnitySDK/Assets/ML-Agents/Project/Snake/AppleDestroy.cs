cdee1db2f49247149a3f1b8d0db687470df04bef
Frederico Ramos <frediramos@gmail.com>
2020-01-17 23:37:57 +0000

    Overall improvements and small fixes

--- Project 4/ml-agents/UnitySDK/Assets/ML-Agents/Project/Snake/AppleDestroy.cs
+++ Project 4/ml-agents/UnitySDK/Assets/ML-Agents/Project/Snake/AppleDestroy.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class AppleDestroy : MonoBehaviour
 {
     public const float DEFAULT_HEIGHT = 0.0f;
     void OnCollisionEnter(Collision collision)
-    {
-        gameObject.transform.position = new Vector3(Random.Range(-7.0f, 7.0f), DEFAULT_HEIGHT, Random.Range(-7.0f, 7.0f));
+    {   
+        if(collision.collider.tag == "wall" || collision.collider.tag == "Player")
+        {
+            //Random position with 0 velocity
+            gameObject.transform.position = new Vector3(Random.Range(-7.0f, 7.0f), DEFAULT_HEIGHT, Random.Range(-7.0f, 7.0f));
+            gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+       
     }
  
 }
